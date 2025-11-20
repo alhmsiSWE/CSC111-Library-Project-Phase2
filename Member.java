f23af9714dd028f0ca7ac169fcfbb2fdcc5b0af8
yazeedalhomoud-prog <yazeedalhomoud@gmail.com>
2025-11-20 17:47:42 +0300

    Member class v1

---
+++ Member.java
@@ -0,0 +1,16 @@
+package library2;
+
+public class Member {
+	private int id;
+	private String name;
+	private int borrowedcount;
+	private int nemViewBorrowed;
+	private int numReturns; 
+	private double sessionFees;
+	public double TotalRevenue;
+	public int TotalViewBorrowed;
+	public int TotalBorrows;
+	public int TotalReturns;
+
+
+}
